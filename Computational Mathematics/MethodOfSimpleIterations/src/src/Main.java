62d2e576b512209c61218577ccb934a9985c5c05
DecafMango <DecafMangoITMO@yandex.ru>
2024-02-17 18:27:28 +0300

    fix: handle cases where it's impossible to achieve a solution

---
+++ Computational Mathematics/MethodOfSimpleIterations/src/src/Main.java
@@ -0,0 +1,36 @@
+package src;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Для выхода из программы напишите exit.");
+
+        InputReader inputReader = new InputReader();
+        Calculation calculation = new Calculation();
+
+        Data data = null;
+        switch (args.length) {
+            case 0:
+                data = inputReader.readFromTerminal();
+                break;
+            case 1:
+                data = inputReader.readFromFile(args[0]);
+                break;
+            default:
+                System.out.println("Программа принимает один аргумент - путь к файлу.");
+                System.exit(1);
+        }
+
+        if (calculation.getDeterminant(data.getA()) == 0) {
+            System.out.println("""
+                    Полученная матрица является вырожденной (определитель равен нулю).
+                    Данная программа работает только со совместными и определенными СЛАУ.""");
+            System.exit(1);
+        }
+
+        calculation.toConvergence(data);
+
+        calculation.iterate(data);
+    }
+
+}
